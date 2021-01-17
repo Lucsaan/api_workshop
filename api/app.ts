ac959f53cf268bc53e317c405709b8e894a26e23
Andreas Schmieder <andreas.schmieder@googlemail.com>
2021-01-17 19:38:06 +0100

    add post

--- api/app.ts
+++ api/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import bodyParser from "body-parser";
 
 let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/test", (req, res, next) => {
     res.json(['Chandran', 'Yasin', 'Andi']);
@@ -8,6 +12,7 @@ app.get("/test", (req, res, next) => {
 
 app.post("/post", (req, res, next) => {
     console.log('request', req.body);
+    res.json({"chandran": "ahahaha"});
 });
 
 app.listen(3000, () => {
